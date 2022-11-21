967e93a498e2614291590937ee3bc6e2b1d5b023
aldemirojunior <lelekojunior18@gmail.com>
2022-11-20 22:19:03 -0300

    first commit

---
+++ src/App.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+require_once (__DIR__.'/.../vendor/autoload.php');
+use App\models\Aluno;
+/**
+ * Description of App
+ *
+ * @author 5511934424010
+ */
+
+    $a = new Aluno();
+
+    var_dump($a);
---
+++ src/models/Aluno.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\models;
+
+/**
+ * Description of Aluno
+ *
+ * @author 5511934424010
+ */
+class Aluno {
+    function __construct() {
+        echo "construtor do aluno rodando...";
+    }
+}
